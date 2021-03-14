211ccbe0b54f5f56f16949e2cd2a2b2c632ef6a4
JD Davis <mxjerrett@gmail.com>
2021-03-14 16:37:18 -0500

    Couple more warnings

--- Src/Application/Common/Interfaces/ICurrentUserService.cs
+++ Src/Application/Common/Interfaces/ICurrentUserService.cs
@@ -1,8 +1,4 @@
-﻿using System.Threading;
-using System.Threading.Tasks;
-using Domain.Models;
-
-namespace Application.Common.Interfaces
+﻿namespace Application.Common.Interfaces
 {
     public interface ICurrentUserService
     {
